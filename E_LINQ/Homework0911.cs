2c50201298f06fe62d52169a2f14e7f906596022
Aleksandr <PITON369@yandex.ru>
2024-01-19 16:19:10 +0300

    Homework0911.

---
+++ E_LINQ/Homework0911.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace E_LINQ
+{
+    public class Homework0911
+    {
+        // Using a file with the TOP100 chess players, find all players from Russia and sort them by year of birth in ascending order.
+        public void HW_FindRusChessPlayers()
+        {
+            string[] strings = File.ReadAllLines(@"Top100ChessPlayers.csv");
+            var players = File.ReadAllLines(@"Top100ChessPlayers.csv")
+                .Skip(1)
+                .Select(Player.Parse)
+                .Where(player=>player.Country=="RUS")
+                .OrderBy(player=>player.BirthDate)
+                .ToList();
+
+            foreach (var player in players)
+            {
+                Console.WriteLine(player);
+            }
+            Console.ReadLine();
+        }
+
+        public class Player
+        {
+            //"1;Carlsen, Magnus;g;NOR;2842;0;1990"
+            public string ID { get; set; }
+            public string Name { get; set; }
+            public string Country { get; set; }
+            public string Rating { get; set; }
+            public string BirthDate { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Name} {Country} {BirthDate}";
+            }
+
+        public static Player Parse(string parts)
+        {
+            string[] line = parts.Split(';');
+            return new Player()
+            {
+                ID = line[0],
+                Name = line[1],
+                Country = line[3],
+                Rating = line[4],
+                BirthDate = line[6]
+            };
+        }
+    }
+    }
+}
---
+++ E_LINQ/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using E_LINQ;
+
+var homework0911 = new Homework0911();
+homework0911.HW_FindRusChessPlayers();
